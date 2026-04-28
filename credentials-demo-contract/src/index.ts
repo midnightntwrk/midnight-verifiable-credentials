d407deb569860c27331bf01f822b5579f66e0186
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2026-04-29 00:26:20 +0800

    Remove passport-compliance from VC demo contract

--- credentials-demo-contract/src/index.ts
+++ credentials-demo-contract/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./managed/demo/contract/index.js";
 export * as CredentialsDemoContract from "./managed/demo/contract/index.js";
-export * as PassportComplianceDemoContract from "./managed/passport-compliance-demo/contract/index.js";
