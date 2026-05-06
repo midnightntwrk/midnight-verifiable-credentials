98496643260a1431b19c059243b4ff2e9ea2b93d
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2026-05-07 05:04:27 +0800

    feat(status): add observed-root freshness helpers

--- credentials-status-registry/src/index.ts
+++ credentials-status-registry/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./attestation-builder.js";
 export * from "./managed/revocation-registry/contract/index.js";
 export * as RevocationRegistryContract from "./managed/revocation-registry/contract/index.js";
+export * from "./registry-state-observation.js";
 export * from "./status-binding.js";
 export * from "./witness-builder.js";
