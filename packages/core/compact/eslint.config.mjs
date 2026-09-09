d66df66d0a0817a4ec95abe3f5009c23579cb41f
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2026-09-09 09:15:41 +0800

    refactor(lint): remove legacy ESLint compatibility
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- packages/core/compact/eslint.config.mjs
+++ packages/core/compact/eslint.config.mjs
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import plugin from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
 import pluginPrettier from "eslint-plugin-prettier";
-import pluginImport from "eslint-plugin-import";
 import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
@@ -17,13 +16,11 @@ export default [
     plugins: {
       "@typescript-eslint": plugin,
       prettier: pluginPrettier,
-      import: pluginImport,
       "simple-import-sort": pluginSimpleImportSort,
     },
     rules: {
       "prettier/prettier": "error",
       "no-unused-vars": "off",
-      "import/no-unused-modules": [1, { unusedExports: true }],
       "no-duplicate-imports": "error",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
--- packages/core/model/eslint.config.mjs
+++ packages/core/model/eslint.config.mjs
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import plugin from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
-import pluginImport from "eslint-plugin-import";
 import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
@@ -21,12 +20,10 @@ export default [
     },
     plugins: {
       "@typescript-eslint": plugin,
-      import: pluginImport,
       "simple-import-sort": pluginSimpleImportSort,
     },
     rules: {
       "no-unused-vars": "off",
-      "import/no-unused-modules": [1, { unusedExports: true }],
       "no-duplicate-imports": "error",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
